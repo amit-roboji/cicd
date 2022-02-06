53f631b50adbef35472e0a9599ba8f460a777cc3
amit-roboji <98898038+amit-roboji@users.noreply.github.com>
2022-02-06 16:40:38 +0530

    Update server.js

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const HOST = '0.0.0.0';
 // App
 const app = express();
 app.get('/', (req, res) => {
-  res.send('Hello World, Hope you are doing well. hope you are well !!');
+  res.send('Hello World, Hope you are doing well. hope you are well. my cicd pipeline is not working  !!');
 });
 
 app.listen(PORT, HOST);
