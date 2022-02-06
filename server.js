3cf92ba77847b0c16a12273d845e0c405ca44157
amit-roboji <98898038+amit-roboji@users.noreply.github.com>
2022-02-06 16:49:45 +0530

    Update server.js

--- server.js
+++ server.js
@@ -9,10 +9,7 @@ const HOST = '0.0.0.0';
 // App
 const app = express();
 app.get('/', (req, res) => {
-  res.send('Hello World, Hope you are doing well. hope you are well. my cicd pipeline is not working 
-Step 1 is to Generate and store Git credentials, click on the link and it will display a command, copy that and run it in your local terminal/command prompt. Now we can access our Cloud Source Repository.
-
-Follow the next steps, Create a folder in your local system, open the terminal/command prompt,  run the git clone command to clone the repository. Now copy the application code files to the clone folder and then run commit and push commands to update the Google Cloud Source Repository. !!');
+  res.send('Hello World, Hope you are doing well. hope you are well. my cicd pipeline is not working vdfgergrethretethdsgtjfdgerjtrhsrhryhdrt!!');
 });
 
 app.listen(PORT, HOST);
